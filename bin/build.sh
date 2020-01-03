e26438619a578e785e08858daba8f5efab292404
Marko Mihelj <marko.mihelj@infinum.hr>
2020-01-03 17:43:36 +0100

    changes

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,6 @@ mkdir ./prod
 mkdir ./prod/build
 
 mv -v build/* prod/build/
-cp app.js config.js folderList.json logger.js helpers.js package.json package-lock.json prod
+cp app.js folderList.json logger.js helpers.js package.json package-lock.json prod
 
 zip -r "torrentBox-$(date +"%Y-%m-%d|%H-%M-%S").zip" prod/*
