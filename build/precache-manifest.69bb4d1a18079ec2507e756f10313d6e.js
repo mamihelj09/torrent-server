465f4de21880f8bd96ad4f73dd1bfd396583ec46
Marko Mihelj <marko.mihelj@infinum.hr>
2020-01-03 18:05:55 +0100

    add build to git

---
+++ build/precache-manifest.69bb4d1a18079ec2507e756f10313d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016d8b8a1d14eea73277397adb747b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664881bcfa2bcff988fa",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "7350ed28c3ff01572160",
+    "url": "/static/css/main.2cbf4547.chunk.css"
+  },
+  {
+    "revision": "664881bcfa2bcff988fa",
+    "url": "/static/js/2.9a676c79.chunk.js"
+  },
+  {
+    "revision": "d7424fac376722fc20a41edf3d7aab51",
+    "url": "/static/js/2.9a676c79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7350ed28c3ff01572160",
+    "url": "/static/js/main.27d6b5f5.chunk.js"
+  },
+  {
+    "revision": "164711751ffebcf78aed",
+    "url": "/static/js/runtime-main.abf965c3.js"
+  }
+]);
